5abafec26a823695714d5d54a211d379416ad4e9
Holly Hoffman <1writecandidate@gmail.com>
2024-11-11 17:06:48 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: '<h1>{{title}}<h1>',
+  styles: ['h1 {color:red;}']
 })
 export class AppComponent {
   title = 'student-list';
